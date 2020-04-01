ac402d333f5c465973a3149902f0a8fe4d18bfc9
Marco Perricone <marco@tomatoweb.net>
2020-04-01 18:38:09 +0200

    Aggiunto Asp.Net Core MVC

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyCourse.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Sono Index");
+        }
+
+        public IActionResult Detail(string id)
+        {
+            return Content($"Sono detail, ho ricevuto l'id {id}");
+        }
+        
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,7 @@ namespace MyCourse
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,11 +28,10 @@ namespace MyCourse
 
             app.UseStaticFiles();
 
-            app.Run(async (context) =>
-            {              
-                string nome = context.Request.Query["nome"];
-                await context.Response.WriteAsync($"Hello {nome}");
-               
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(routeBuiler =>
+            {
+                routeBuiler.MapRoute("default", "{controller=Home}/{action=index}/{id?}");
             });
         }
     }
